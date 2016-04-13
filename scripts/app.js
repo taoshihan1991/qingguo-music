1fcc2c732f51cee908bfb45ee0ebc6c52c1bf878
taoshihan <630892807@qq.com>
2016-04-13 09:50:31 +0800

    修改报错部分和刷新问题

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,31 @@
 var app=angular.module("Home",["ui.router"]);
-window.location.href="#/";
+window.location.href="#/index";
+var ap1 = new APlayer({
+    element: document.getElementById('player1'),
+    narrow: false,
+    autoplay: true,
+    showlrc: false,
+    mutex: true,
+    theme: '#C70C0C',
+    music: {
+        title: '在他乡',
+        author: '水木年华',
+        url: 'http://sc.111ttt.com/up/mp3/110892/2466AEB3911F8D8308C45EB29E3F9B95.mp3',
+        pic: 'http://img1.kuwo.cn/star/starheads/240/79/78/2507339289.jpg'
+    }
+});
+ap1.init();
+    
 app.config(function($stateProvider,$urlRouterProvider){
+
+
 	//音乐首页
-    $stateProvider.state("/",{
-        url:"/",
+    $stateProvider.state("index",{
+        url:"/index",
         templateUrl:'views/musicIndex.html',
         controller:'musicIndex'
     });
-	//音乐列表
+	//
     $stateProvider.state("recommend",{
         url:"/recommend",
         templateUrl:'views/recommend.html',
--- scripts/controllers/index.js
+++ scripts/controllers/index.js
@@ -1,8 +1,19 @@
 //音乐首页
 app.controller("musicIndex",function($scope){
+
 });
 
-//音乐列表
+//推荐
 app.controller("recommend",function($scope){
+	
+});
+
+//排行榜
+app.controller("topList",function($scope){
+	
+});
+
+//歌单
+app.controller("playList",function($scope){
 	
 });

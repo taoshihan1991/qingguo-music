98b028f17c13ad1a932725d510ec8564c2a793f1
taoshihan <630892807@qq.com>
2016-04-15 00:13:23 +0800

    首页循环列表加判断

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,70 @@
 var app=angular.module("Home",["ui.router"]);
 window.location.href="#/index";
+
+var musicJson=[
+        {
+            title: '一直很安静,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260494.mp3',
+            pic: 'images/2.jpg'
+        },
+        {
+            title: '终于明白,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260505.mp3',
+            pic: 'images/3.jpg'
+        },
+        {
+            title: '六月的雨',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/200760.mp3',
+            pic: 'images/4.jpg'
+        },
+        {
+            title: '一直很安静,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260494.mp3',
+            pic: 'images/2.jpg'
+        },
+        {
+            title: '终于明白,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260505.mp3',
+            pic: 'images/3.jpg'
+        },
+        {
+            title: '六月的雨',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/200760.mp3',
+            pic: 'images/4.jpg'
+        },
+        {
+            title: '一直很安静,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260494.mp3',
+            pic: 'images/2.jpg'
+        },
+        {
+            title: '终于明白,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260505.mp3',
+            pic: 'images/3.jpg'
+        },
+        {
+            title: '六月的雨',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/200760.mp3',
+            pic: 'images/4.jpg'
+        },
+        {
+            title: '一直很安静,电视剧《仙剑奇侠传》插曲',
+            author: '《仙剑奇侠传》电视剧',
+            url: 'http://link.hhtjim.com/163/5260494.mp3',
+            pic: 'images/2.jpg'
+        },
+
+    ];
+
 var ap1 = new APlayer({
     element: document.getElementById('player1'),
     narrow: false,
@@ -7,20 +72,7 @@ var ap1 = new APlayer({
     showlrc: false,
     mutex: true,
     theme: '#C70C0C',
-    music:[
-        {
-            title: '一直很安静',
-            author: '《仙剑奇侠传》电视剧',
-            url: 'http://link.hhtjim.com/163/5260494.mp3',
-            pic: 'images/2.jpg'
-        },
-        {
-            title: '终于明白',
-            author: '《仙剑奇侠传》电视剧',
-            url: 'http://link.hhtjim.com/163/5260505.mp3',
-            pic: 'images/3.jpg'
-        }
-    ]
+    music:musicJson
 });
 ap1.init();
     
--- scripts/controllers/index.js
+++ scripts/controllers/index.js
@@ -1,6 +1,20 @@
 //音乐首页
 app.controller("musicIndex",function($scope){
 
+	$scope.musicList=musicJson;
+	$scope.play=function(index){
+		var ap1 = new APlayer({
+		    element: document.getElementById('player1'),
+		    narrow: false,
+		    autoplay: true,
+		    showlrc: false,
+		    mutex: true,
+		    theme: '#C70C0C',
+		    music:musicJson[index]
+		});
+		ap1.init();
+	}
+
 });
 
 //推荐

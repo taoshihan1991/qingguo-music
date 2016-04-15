2b30bdf97a832a60cd318277facbdf0f634647d9
taoshihan <630892807@qq.com>
2016-04-15 14:24:44 +0800

    图片延迟

--- apis/playlist.php
+++ apis/playlist.php
@@ -1,6 +1,6 @@
 <?php
 // 歌单的id
-$neteast=file_get_contents("http://music.163.com/api/playlist/detail?id=362664951");
+$neteast=https_request("http://music.163.com/api/playlist/detail?id=362664951");
 $neteastJson=json_decode($neteast,true);
 if($neteastJson['code']==200){
 	$songs=$neteastJson['result']['tracks'];
@@ -15,4 +15,20 @@ if($neteastJson['code']==200){
 	}
 
 	echo json_encode(($result));
+}
+
+
+function https_request($url,$data = null){
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+	if (!empty($data)){
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	}
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	$output = curl_exec($curl);
+	curl_close($curl);
+	return $output;
 }
